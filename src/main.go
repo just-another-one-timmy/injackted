328bfbc9226d4f7722688c4c4b0ef35183b1c209
Iaroslav Tymchenko <jtimchenko@gmail.com>
2013-05-21 20:13:06 +0300

    Stupid client version 2. To be continued.

--- src/main.go
+++ src/main.go
@@ -3,21 +3,58 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
+	"regexp"
 	"structs"
 )
 
-func BuildIndex() *structs.Index {
+func addKeywords(index *structs.Index, doc string, keywords []string) {
+	for _, keyword := range keywords {
+		index.ConnectKeywordDoc(keyword, doc)
+	}
+}
+
+func getKeywordsFromFile(file string) []string {
+	// TODO(iaroslav): write actual code.
+	return []string{"one", "two", "three", "four", "five"}
+}
+
+func BuildIndex() (*structs.Index, error) {
 	result := structs.NewIndex()
-	return result
+
+	dir, err := os.Open(".")
+	if err != nil {
+		return nil, err
+	}
+
+	names, err := dir.Readdirnames(0)
+	if err != nil {
+		return nil, err
+	}
+
+	rexp, err := regexp.Compile(".*-test")
+	if err != nil {
+		return nil, err
+	}
+
+	for _, name := range names {
+		if rexp.MatchString(name) {
+			addKeywords(result, name, getKeywordsFromFile(name))
+		}
+	}
+
+	return result, nil
 }
 
 func main() {
 	flag.Parse()
 
-	index := BuildIndex()
-	index.ConnectKeywordDoc("ABBA", 1)
-	index.ConnectKeywordDoc("ABBA", 2)
-	index.ConnectKeywordDoc("Beatles", 3)
+	index, err := BuildIndex()
+
+	if err != nil {
+		fmt.Printf("Error while building index: %v.\n", err.Error())
+		return
+	}
 
 	for _, query := range flag.Args() {
 		docs := index.GetDocsByKeyword(query)
