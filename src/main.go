051912062484320eb197b758691fe597251285e4
Iaroslav Tymchenko <jtimchenko@gmail.com>
2013-05-23 03:55:50 +0300

    Rewrote client.

--- src/main.go
+++ src/main.go
@@ -1,82 +1,81 @@
 package main
 
 import (
-	"flag"
 	"fmt"
-	"io/ioutil"
-	"os"
-	"regexp"
-	"strings"
 	"structs"
 )
 
-const WORD_SEPS = " \n\t\\.*"
+func loadIndex(index *structs.Index, fileName string) {
+	//TODO(iaroslav): write real code.
+}
 
-func addKeywords(index *structs.Index, doc string, keywords []string) {
-	for _, keyword := range keywords {
-		keyword = strings.Trim(keyword, WORD_SEPS)
-		index.ConnectKeywordDoc(keyword, doc)
-		fmt.Printf("Connected %v and %v\n", keyword, doc)
+func saveIndex(index *structs.Index, fileName string) {
+	//TODO(iaroslav): write real code.
+}
+
+func connect(index *structs.Index, keyword string, doc string) {
+	index.ConnectKeywordDoc(keyword, doc)
+	fmt.Printf("%q and %q were connected in the index\n", keyword, doc)
+}
+
+func listDocs(index *structs.Index, keyword string) {
+	fmt.Printf("Documents which contain keyword %q:\n", keyword)
+	for doc, _ := range *index.GetDocsByKeyword(keyword).IterableMap() {
+		fmt.Printf("\t%v\n", doc)
 	}
 }
 
-func getKeywordsFromFile(fileName string) []string {
-	fmt.Printf("%v\n", fileName)
-	fileContents, err := ioutil.ReadFile(fileName)
-	if err != nil {
-		fmt.Printf("Error: %v\n", err.Error())
-		return nil
+func listKeywords(index *structs.Index, doc string) {
+	fmt.Printf("Keywords contained in %q:\n", doc)
+	for keyword, _ := range *index.GetKeywordsByDoc(doc).IterableMap() {
+		fmt.Printf("\t%v\n", keyword)
 	}
-	fmt.Printf("%v\n", string(fileContents))
-	return strings.Split(string(fileContents), " ")
 }
 
-func BuildIndex() (*structs.Index, error) {
-	result := structs.NewIndex()
-	const base_dir = "./test_inputs/"
-
-	dir, err := os.Open(base_dir)
-	if err != nil {
-		return nil, err
+func handleCommand(command string, index *structs.Index) {
+	switch command {
+	case "help":
+		fmt.Printf("Available commands:\n" +
+			"help:                       displays help\n" +
+			"load <filename>:            loads index from <filename>\n" +
+			"save <filename>:            saves index to <filename>\n" +
+			"connect <keyword> <doc>:    connects <keyword> and <doc> in index\n" +
+			"list-docs <keyword>:        lists documents containing <keyword>\n" +
+			"list-keywords <doc>:        lists keywords contained in <doc>\n")
+	case "load":
+		var arg string
+		fmt.Scan(&arg)
+		loadIndex(index, arg)
+	case "save":
+		var arg string
+		fmt.Scan(&arg)
+		saveIndex(index, arg)
+	case "connect":
+		var arg1, arg2 string
+		fmt.Scan(&arg1, &arg2)
+		connect(index, arg1, arg2)
+	case "list-docs":
+		var arg string
+		fmt.Scan(&arg)
+		listDocs(index, arg)
+	case "list-keywords":
+		var arg string
+		fmt.Scan(&arg)
+		listKeywords(index, arg)
+	default:
+		fmt.Printf("Unrecognized command: %q\n", command)
 	}
-
-	names, err := dir.Readdirnames(0)
-	if err != nil {
-		fmt.Printf("Error: %v\n", err.Error())
-		return nil, err
-	}
-
-	rexp, err := regexp.Compile(".*-test")
-	if err != nil {
-		fmt.Printf("Error: %v\n", err.Error())
-		return nil, err
-	}
-
-	for _, name := range names {
-		fmt.Printf("Found file %v\n", name)
-		if rexp.MatchString(name) {
-			addKeywords(result, base_dir+name, getKeywordsFromFile(base_dir+name))
-		}
-	}
-
-	return result, nil
 }
 
 func main() {
-	flag.Parse()
-
-	index, err := BuildIndex()
-
-	if err != nil {
-		fmt.Printf("Error while building index: %v.\n", err.Error())
-		return
-	}
-
-	for _, query := range flag.Args() {
-		docs := index.GetDocsByKeyword(query)
-		fmt.Printf("Docs for keyword %v:\n", query)
-		for doc, _ := range *docs.IterableMap() {
-			fmt.Printf("\t%v\n", doc)
+	index := structs.NewIndex()
+	for {
+		fmt.Println("Enter command (e.g. 'help') and arguments.")
+		var userInput string
+		_, err := fmt.Scan(&userInput)
+		if err != nil {
+			fmt.Println(err.Error())
 		}
+		handleCommand(userInput, index)
 	}
 }
